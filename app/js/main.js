11bc9bfbfd97a18da2a32a4ce59369be0c6c6e9f
Alexey Melentev <38543167+AllXeight@users.noreply.github.com>
2019-07-31 16:51:19 +0300

    final version 1.0

---
+++ app/js/main.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    //swiper->
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    $(".nav li").on("click", function () {      
+        reinitializeSwiper(swiper);
+    });
+
+    function reinitializeSwiper(swiper) {
+        setTimeout(function () {
+            swiper.update();
+        }, 200);
+    };
+    //->swiper
+});
